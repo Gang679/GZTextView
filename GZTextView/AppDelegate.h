e4514b655c5d6d43803999f66d84989c6c167b0e
Gang679 <xinshijie@xinshijietekiimac.local>
2016-07-12 15:47:24 +0800

    Initial Commit

---
+++ GZTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZTextView
+//
+//  Created by xinshijie on 16/7/12.
+//  Copyright © 2016年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZTextView
+//
+//  Created by xinshijie on 16/7/12.
+//  Copyright © 2016年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
